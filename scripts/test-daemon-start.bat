ee4dfd783be8fd6d852a035a2ff45447acab2809
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2017-11-15 17:11:55 +0100

    Get PID

--- scripts/test-daemon-start.bat
+++ scripts/test-daemon-start.bat
@@ -2,7 +2,7 @@
 echo "Starting hyperg"
 start hyperg.exe
 
-for /F "TOKENS=1,2,*" %a in ('tasklist /FI "IMAGENAME eq hyperg.exe"') do set H_PID=%b
+for /F "TOKENS=2" %a in ('tasklist /nh /fi "IMAGENAME eq hyperg.exe"') do set H_PID=%a
 echo %H_PID% > .test-daemon-hyperg.pid
 
 exit 0
