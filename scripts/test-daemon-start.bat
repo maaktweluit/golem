e7cee88cdf7fe38ef380b283235ee9f0154add63
maaktweluit <10008353+maaktweluit@users.noreply.github.com>
2017-11-15 16:38:42 +0100

    Start in background

--- scripts/test-daemon-start.bat
+++ scripts/test-daemon-start.bat
@@ -1,6 +1,6 @@
 
 echo "Starting hyperg"
-hyperg.exe &
+start hyperg.exe
 
 for /F "TOKENS=1,2,*" %a in ('tasklist /FI "IMAGENAME eq hyperg.exe"') do set H_PID=%b
 echo %H_PID% > .test-daemon-hyperg.pid
